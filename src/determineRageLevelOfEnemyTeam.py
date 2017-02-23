eff56a1a33a9f0e1aef7cd3652b108abb8dc172c
PsychBear <nmaeys@hotmail.com>
2017-02-22 19:58:42 -0600

    updated first pass of rage level with sample file of rage levels per champion id

--- src/determineRageLevelOfEnemyTeam.py
+++ src/determineRageLevelOfEnemyTeam.py
@@ -1,17 +1,28 @@
 import json
 from parseInGameData import *
 
-inGameData = InGameData.Main()
 
-rageLevels = json.load(open('./sample_rage_level.json'))
+class RageLevels:
 
-enemyChampionIds = inGameData[1]
+	@staticmethod
+	def rageLevels(enemyList, championRageData):
 
-rageLevelList = []
-for enemy in enemyChampionIds:
-	rageLevelList.append(int((rageLevels["championIds"][str(enemy)]["rageLevel"])) / 10.0)
+		rageLevelList = []
+		for enemy in enemyList:
+			rageLevelList.append(int((championRageData["championIds"][str(enemy)]["rageLevel"])) / 10.0)
+
+		chanceOfRage = ((sum(rageLevelList) / len(rageLevelList)) * 100)
+		return chanceOfRage
 
 
-chanceOfRage = ((sum(rageLevelList) / len(rageLevelList)) * 100)
+	@staticmethod
+	def Main():
+		championRageData = json.load(open('./sample_rage_level.json'))
+		inGameData = InGameData.Main()
+		chanceOfRage = RageLevels.rageLevels(inGameData[1], championRageData)
 
-print "There is a %" + str(chanceOfRage) + " chance of rage."
+		print "There is a %" + str(chanceOfRage) + " chance of rage."
+
+if __name__ == "__main__":
+
+	RageLevels.Main()
